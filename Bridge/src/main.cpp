4163263186852956a5f45a18b23305e9d1307038
David Thacher <davidethacher@gmail.com>
2023-12-08 12:38:58 -0500

    Add forth

--- Bridge/src/main.cpp
+++ Bridge/src/main.cpp
@@ -14,19 +14,23 @@
 static uint8_t vector0[32 * 1024];
 static uint8_t vector1[32 * 1024];
 static uint8_t vector2[32 * 1024];
+static uint8_t vector3[32 * 1024];
 
 static void __not_in_flash_func(loop)() {
     BitBang in0(vector0, sizeof(vector0), 1);
     BitBang in1(vector1, sizeof(vector1), 1);
     BitBang in2(vector2, sizeof(vector2), 1);
+    BitBang in3(vector3, sizeof(vector3), 1);
     SPI out0(&in0);
     SPI out1(&in1);
     SPI out2(&in2);
+    SPI out3(&in3);
 
     while (1) {
         out0.write();
         out1.write();
         out2.write();
+        out3.write();
 
         watchdog_update();          // We are only interested in protecting core 0
     }
